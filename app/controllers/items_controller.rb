843b31537f7c683e3658e3f5cdda1742f710cb55
konpeisan <introjection.19@icloud.com>
2021-05-27 01:38:24 +0900

    商品情報入力画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:image,:items_name,:items_text,:category_id,:status_id,:delivery_id,:area_id,:shopping_day_id,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to       :category_id
+  belongs_to       :status_id
+  belongs_to       :delivery_id
+  belongs_to       :area_id
+  belongs_to       :shopping_day_id
+
   has_one_attached :image
   belongs_to       :user
   # has_many         :comments
   # has_one          :history
+
+  with_options presence: true, numericality: {other_than: 1} do
+    validates :items_name
+    validates :items_text
+    validates :category_id
+    validates :status_id
+    validates :delivery_id
+    validates :area_id
+    validates :shopping_day_id
+    validates :price
+  end
 end
