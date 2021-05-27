ecb6075065157ea5bc50d7bffadccc2ab773b6d0
konpeisan <introjection.19@icloud.com>
2021-05-27 14:14:14 +0900

    金額指定の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,28 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to       :category_id
-  belongs_to       :status_id
-  belongs_to       :delivery_id
-  belongs_to       :area_id
-  belongs_to       :shopping_day_id
+  belongs_to       :category
+  belongs_to       :status
+  belongs_to       :delivery
+  belongs_to       :area
+  belongs_to       :shopping_day
 
   has_one_attached :image
   belongs_to       :user
   # has_many         :comments
   # has_one          :history
 
-  with_options presence: true, numericality: {other_than: 1} do
+  with_options presence: true do
     validates :items_name
     validates :items_text
-    validates :category_id
-    validates :status_id
-    validates :delivery_id
-    validates :area_id
-    validates :shopping_day_id
-    validates :price
+    with_options numericality: {other_than: 1} do
+      validates :category_id
+      validates :status_id
+      validates :delivery_id
+      validates :area_id
+      validates :shopping_day_id
+    end
+    with_options format: {with: /\A[0-9]+\z/} do
+      validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message:"300~9,999,999の間で指定してください"}
+    end
   end
 end
