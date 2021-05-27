c0a519580ae87249331f83c318d9555da1adaf7b
konpeisan <introjection.19@icloud.com>
2021-05-27 22:04:52 +0900

    ログアウト中にURLから遷移するとログインページに遷移する機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to action: new_user_session_path
     end
   end
 end
