385011d6f9b723eb1a57f918aee47c16fcf39f00
konpeisan <introjection.19@icloud.com>
2021-05-28 11:14:43 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create,:edit,:update]
-  before_action :move_to_index, except: [:index,:show]
+  before_action :item_find, only: [:show,:edit,:update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user == @item.user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -42,9 +39,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:items_name,:items_text,:category_id,:status_id,:delivery_id,:area_id,:shopping_day_id,:price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: new_user_session_path
-    end
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
