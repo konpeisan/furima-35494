9c3e6c396a7f191ed53fd95ed22af57bb2acbe93
konpeisan <introjection.19@icloud.com>
2021-06-05 11:21:33 +0900

    commentモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,8 @@ class User < ApplicationRecord
   end
 
   has_many :items
-  # has_many :comments
-  # has_many :histories
+  has_many :comments
+  has_many :histories
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
